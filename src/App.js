02ae2a1fc97443f1e331562f58c9c4639b6ca824
Vijayendra Dev <bn.vijayendradev@gmail.com>
2024-02-29 02:04:33 +0530

    completed responsive landing page

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import Herosection from '../src/Pages/Herosection'
+import Features from '../src/Pages/Features'
+import Footer from '../src/Pages/Footer'
+
+
+function App() {
+  return (
+    <>
+    <Herosection />
+    <Features />
+    <Footer />
+    </>
+    
+  );
+}
+
+export default App;
